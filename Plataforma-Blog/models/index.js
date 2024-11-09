559111a70efeea2652b28317fcdf93ba7136278b
Prisma74 <solopipe2001@gmail.com>
2024-11-08 21:21:04 -0500

    Create index.js

---
+++ Plataforma-Blog/models/index.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+const config = require('../config/config').development;
+
+const sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    {
+        host: config.host,
+        dialect: config.dialect,
+        port: config.port
+    }
+);
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.User = require('./user')(sequelize, Sequelize);
